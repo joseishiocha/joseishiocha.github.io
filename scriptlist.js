ef8b7a75446f98b3645ab90a89dea4b01f648285
joseishiocha <joseishi.ocha@gmail.com>
2026-08-15 21:37:06 +0900

    0815

---
+++ scriptlist.js
@@ -0,0 +1,26 @@
+const books = [
+    {
+        title: "Women Composers: The Lost Tradition Found",
+        author: "Diane Peacock Jezic",
+        theme: "音楽",
+        status: "発表者募集中"
+    },
+    {
+        title: "Women & music : a history",
+        author: "Karin Pendle",
+        theme: "音楽",
+        status: "発表者募集中"
+    },
+    {
+        title: "“Am I That Name?” Feminism and the Category of \"Women\" in History",
+        author: "Denise Riley",
+        theme: "フェミニズム",
+        status: "発表者募集中"
+    },
+    {
+        title: "",
+        author: "○○",
+        theme: "明治女性史",
+        status: "発表予定"
+    }
+];
